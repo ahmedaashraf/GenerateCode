61e159805c4e65176b57c7f865e6c065272bc11e
Ahmed Ashraf Taha <37928650+ahmedaashraf@users.noreply.github.com>
2021-05-19 12:05:53 +0200

    Update Script

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function generate() {
-    var input = document.getElementById("data").value;
-    link = "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl="+input;
-    open(link);
+    var phonenumber = document.getElementById("data").value;
+    var message = document.getElementById("data2").value
+    userAction(phonenumber,message)
+
 }
+
+const userAction = async (phonenumber,message) => {
+    const response = await fetch('https://chart.googleapis.com/chart?', {
+      method: 'POST',
+      body: {
+          "cht":"qr",
+          "chs":"200x200",
+          "chl": "wa.me/2"+phonenumber+"?text"+message
+      }, 
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'https://ahmedaashraf.github.io/GenerateCode/QRcode.html',
+        'Access-Control-Allow-Credentials':'true'
+      }
+    });
+   
+    const myJson = await response.json(); //extract JSON from the http response
+    console.log(myJson)
+  }
