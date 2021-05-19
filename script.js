55d9132e81a4020ef015b04dee319a022f7bc9ed
Ahmed Ashraf Taha <37928650+ahmedaashraf@users.noreply.github.com>
2021-05-19 16:02:34 +0200

    Upload

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function generate() {
-    var phonenumber = document.getElementById("data").value;
-    var message = document.getElementById("data2").value
-    var WAlink = "wa.me/2"+phonenumber+"?text="+message
-    
-    link = "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl="+WAlink;
+    var phonenumber = document.getElementById("phonenumber").value;
+    var message = document.getElementById("message").value
+    var WAlink = "http://wa.me/2"+phonenumber+"?text="+message
+
+    link = "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl="+ WAlink;
     open(link);
-    
 }
 
