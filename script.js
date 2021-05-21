9873070c6524089231d45eb8104bf33664074d52
Ahmed Ashraf Taha <37928650+ahmedaashraf@users.noreply.github.com>
2021-05-21 19:51:24 +0200

    Update Script

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 function generate() {
     var phonenumber = document.getElementById("phonenumber").value;
-    var message = document.getElementById("message").value
-    var WAlink = "http://wa.me/2"+phonenumber+"?text="+message
+    var message = document.getElementById("message").value;
+    var file = document.getElementById("file").files[0];
 
-    link = "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl="+ WAlink;
-    open(link);
-}
 
+    var WAlink = "http://wa.me/2"+phonenumber+"?text="+message;
+
+    const formData = new FormData();
+    
+    if (file != null){
+       formData.append('file', file);
+    }
+    formData.append('link', WAlink);
+    formData.append('size',10);
+    
+    getcode(formData);
+ 
+};
+
+async function getcode (formData) {
+	    await fetch('https://qrcodenotch.herokuapp.com/generate/api/v1.0/', { 
+	    method: 'POST',
+	    headers: {
+
+	    },
+	    body: formData 
+	  }).then(
+	    response => response.json() // if the response is a JSON object
+	  ).then(
+	    success => console.log(success) // Handle the success response object
+	  ).catch(
+	    error => console.log(error) // Handle the error response object
+	  );
+ };
