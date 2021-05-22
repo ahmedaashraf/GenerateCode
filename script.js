9505dd840176076bb389e4e685ec4814e37a625c
Ahmed Ashraf Taha <37928650+ahmedaashraf@users.noreply.github.com>
2021-05-22 13:57:35 +0200

    Fixed Headers , and Added download QR CODE

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
-function generate() {
+async function generate(e) {
+    e.preventDefault();
     var phonenumber = document.getElementById("phonenumber").value;
     var message = document.getElementById("message").value;
     var file = document.getElementById("file").files[0];
-
+    var size = 10
+    console.log(file)
 
     var WAlink = "http://wa.me/2"+phonenumber+"?text="+message;
-
+    var a = document.createElement("a");
     const formData = new FormData();
     
-    if (file != null){
-       formData.append('file', file);
+     if (file != null){
+       formData.append('file_image', file);
     }
-    formData.append('link', WAlink);
-    formData.append('size',10);
     
-    getcode(formData);
- 
-};
-
-async function getcode (formData) {
-	    await fetch('https://qrcodenotch.herokuapp.com/generate/api/v1.0/', { 
+    
+     await fetch('https://qrcodenotch.herokuapp.com/generate/api/v1.0/?size='+size+'&link='+WAlink, { 
 	    method: 'POST',
+	    mode: 'cors',
+    	    cache: 'default',
+	    body: formData, 
 	    headers: {
 
-	    },
-	    body: formData 
+		
+	    }
 	  }).then(
-	    response => response.json() // if the response is a JSON object
+	    response => response.blob()// if the response is a JSON object
 	  ).then(
-	    success => console.log(success) // Handle the success response object
+	    blob => {
+  		const objectURL = URL.createObjectURL(blob);
+ 		a.href = objectURL;
+        	a.download = 'DownloadedQRCODE';
+        	a.click();
+        	window.URL.revokeObjectURL(objectURL);
+ 		}
 	  ).catch(
 	    error => console.log(error) // Handle the error response object
 	  );
- };
+
+ 
+};
+
+
